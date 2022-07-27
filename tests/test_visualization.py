2607b7ca2dbc5e874cf9366eb804f0fa5bbf2ba5
Dylan Vasey <dyvasey@gmail.com>
2022-07-27 09:57:47 -0700

    Renamed pyvista visualization module

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -5,21 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyvista as pv
 
-from gdmate import visualization
+from gdmate.visualization import pyvista_vis
 
 # Create very small mesh of 9 cells with 16 points and assign each a value
 mesh = pv.UniformGrid(dims=(4, 4, 1)).cast_to_unstructured_grid()
 mesh['sample_field'] = np.arange(9)
 
-def test_plot_pyvista_2d():
-    """ Test plot_pyvista_2d function """
+def test_pv_plot_2d():
+    """ Test pv_plot_2d function """
     
     # Create Matplotlib figure/axes
     fig,ax = plt.subplots(1)
 
     # Attempt to plot the mesh on the axes, colored by the sample field and with
     # bounds restricted to 0-2 on the x and y axes.
-    ax = visualization.plot_pyvista_2d(mesh,'sample_field',bounds=[0,2,0,2],
+    ax = pyvista_vis.pv_plot_2d(mesh,'sample_field',bounds=[0,2,0,2],
                                        ax=ax)
 
     # Test that the figure contains 1 axes
